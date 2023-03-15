a6b411399a9ac442a44781ecb48da152d66d670c
William Gardner <william@gardna.net>
2023-03-15 14:06:37 -0700

    Made audio player functional

--- assets/js/player_buttons.js
+++ assets/js/player_buttons.js
@@ -1,3 +1,21 @@
+function updatePlayer(selected) {
+    $("._player_name").text(selected.title);
+    $("#_player_album").attr("src", selected.cover);
+}
+
+function playMusic(source) {
+    var audioPlayer = document.createElement("audio");
+    audioPlayer.setAttribute("src", source);
+    audioPlayer.setAttribute("autoplay", "autoplay");
+    audioPlayer.load();
+
+    audioPlayer.addEventListener("load", () => {
+        audioPlayer.play();
+    });
+
+    return audioPlayer;
+}
+
 $(() => {
     $("._player_button").click(function () {
         var which = $(this).attr("which");
@@ -13,8 +31,45 @@ $(() => {
         }, 500);
     });
 
-    $("#_player_btn_back").click(() => {});
-    $("#_player_btn_forward").click(() => {});
-    $("#_player_btn_play").click(() => {});
-    $("#_player_btn_pause").click(() => {});
+    var playerIndex = Math.floor(Math.random() * PLAYER_TRACKS.length);
+    var playerSelected = PLAYER_TRACKS[playerIndex];
+    var playerPlaying = false;
+    var audioPlayer = null;
+    updatePlayer(playerSelected);
+
+    $("#_player_btn_back").click(() => {
+        if (audioPlayer) audioPlayer.pause();
+        playerPlaying = false;
+
+        playerIndex--;
+        if (playerIndex < 0) playerIndex = PLAYER_TRACKS.length - 1;
+
+        playerSelected = PLAYER_TRACKS[playerIndex];
+
+        updatePlayer(playerSelected);
+        audioPlayer = playMusic(playerSelected.url);
+    });
+
+    $("#_player_btn_forward").click(() => {
+        if (audioPlayer) audioPlayer.pause();
+        playerPlaying = false;
+        playerIndex = (playerIndex + 1) % PLAYER_TRACKS.length;
+        playerSelected = PLAYER_TRACKS[playerIndex];
+
+        updatePlayer(playerSelected);
+        audioPlayer = playMusic(playerSelected.url);
+    });
+
+    $("#_player_btn_play").click(() => {
+        if (playerPlaying) {
+            audioPlayer.play();
+        } else {
+            audioPlayer = playMusic(playerSelected.url);
+            playerPlaying = true;
+        }
+    });
+
+    $("#_player_btn_pause").click(() => {
+        audioPlayer.pause();
+    });
 });
