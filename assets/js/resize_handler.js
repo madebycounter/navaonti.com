89d6bb2fcb8c1e988e7de8a78b9a8bb34813a207
William Gardner <william@gardna.net>
2023-03-29 14:45:48 -0700

    Added interface border assets

--- assets/js/resize_handler.js
+++ assets/js/resize_handler.js
@@ -1,6 +1,15 @@
+function resizeBorder() {
+    var width = $(".interface").width();
+    var height = $(".interface").height();
+
+    $("#border").width(width - 20);
+    $("#border").height(height - 5);
+}
+
 function resizeHandler() {
     updateBannerClipMask();
     updateDesktopClipMask();
+    resizeBorder();
 }
 
 $(window).on("resize", resizeHandler);
@@ -8,4 +17,8 @@ $(window).on("load", () => {
     updateTime();
     resizeHandler();
     $(".loader").fadeOut();
+});
+
+$(".interface").on("load", () => {
+    resizeBorder();
 });
